f3a0c228c322f80304dfa2a39d1084cb3cdbc0c7
Ivan German <ivan.german@opower.com>
2016-03-11 13:59:16 +0200

    Fix incorrect order of operations in DataSourceBasedStatementExecutor#shutdown() method.

--- src/test/java/com/opower/persistence/jpile/AbstractIntTestForJPile.java
+++ src/test/java/com/opower/persistence/jpile/AbstractIntTestForJPile.java
@@ -1,7 +1,8 @@
 package com.opower.persistence.jpile;
 
-import com.google.common.base.Throwables;
 import com.google.common.collect.ImmutableList;
+import com.opower.persistence.jpile.jdbc.ConnectionBasedStatementExecutor;
+import com.opower.persistence.jpile.jdbc.StatementExecutor;
 import com.opower.persistence.jpile.loader.HierarchicalInfileObjectLoader;
 import org.junit.After;
 import org.junit.AfterClass;
@@ -16,8 +17,9 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.jdbc.JdbcTestUtils;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.util.List;
+
+import static com.opower.persistence.jpile.util.JdbcTestUtil.openNewConnection;
 
 /**
  * Abstract test case for all int tests. Loads MySQL drivers and creates a new MySQL {@link Connection}
@@ -27,28 +29,27 @@ import java.util.List;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration
 public abstract class AbstractIntTestForJPile {
-    private static final String JDBC_URL = "jdbc:mysql://localhost/jpile?useUnicode=true&characterEncoding=utf-8";
     private static final List<String> TABLES =
             ImmutableList.of("customer", "product", "contact", "contact_phone", "binary_data", "supplier");
-    private static final String DB_USER = "root";
-    private static final String DB_PASSWORD = "";
 
-    static {
-        try {
-            Class.forName("com.mysql.jdbc.Driver");
-        }
-        catch (ClassNotFoundException e) {
-            throw Throwables.propagate(e);
-        }
-    }
-
+    /**
+     * Free connection that can be used to execute standalone queries
+     */
     protected Connection connection;
+
+    /**
+     * Infile object loader.
+     */
     protected HierarchicalInfileObjectLoader hierarchicalInfileObjectLoader = new HierarchicalInfileObjectLoader();
+
+    /**
+     * Spring's JDBC template wrapped around {@link #connection}
+     */
     protected JdbcTemplate jdbcTemplate;
 
     @BeforeClass
     public static void createTables() throws Exception {
-        Connection connection = DriverManager.getConnection(JDBC_URL, DB_USER, DB_PASSWORD);
+        Connection connection = openNewConnection();
         JdbcTestUtils.executeSqlScript(
                 new JdbcTemplate(new SingleConnectionDataSource(connection, true)),
                 new InputStreamResource(AbstractIntTestForJPile.class.getResourceAsStream("/jpile.sql")),
@@ -59,8 +60,11 @@ public abstract class AbstractIntTestForJPile {
 
     @Before
     public void setUp() throws Exception {
-        this.connection = DriverManager.getConnection(JDBC_URL, DB_USER, "");
-        this.hierarchicalInfileObjectLoader.setConnection(this.connection);
+        this.connection = openNewConnection();
+
+        Connection statementExecutorConnection = openNewConnection();
+        this.hierarchicalInfileObjectLoader.setStatementExecutor(getStatementExecutor(statementExecutorConnection));
+
         this.jdbcTemplate = new JdbcTemplate(new SingleConnectionDataSource(this.connection, true));
     }
 
@@ -75,11 +79,23 @@ public abstract class AbstractIntTestForJPile {
 
     @AfterClass
     public static void dropTables() throws Exception {
-        Connection connection = DriverManager.getConnection(JDBC_URL, DB_USER, "");
+        Connection connection = openNewConnection();
         JdbcTemplate template = new JdbcTemplate(new SingleConnectionDataSource(connection, true));
         for (String table : TABLES) {
             template.update("drop table " + table);
         }
         connection.close();
+    }
+
+    /**
+     * Prepares custom {@link StatementExecutor} to {@link #hierarchicalInfileObjectLoader}.
+     * Default implementation uses {@link ConnectionBasedStatementExecutor} which can be changed by
+     * overriding this method.
+     *
+     * @param connection connection to the database
+     * @return {@link StatementExecutor} instance
+     */
+    public StatementExecutor getStatementExecutor(Connection connection) {
+        return new ConnectionBasedStatementExecutor(connection);
     }
 }
